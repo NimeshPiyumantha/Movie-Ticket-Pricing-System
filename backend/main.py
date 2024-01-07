218c8d804d205927da453e6daf86a3225471bf5d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2024-01-07 11:34:30 +0530

    Add mongodb - Backend

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+app = FastAPI()
+
+# Get MongoDB URI from environment variables
+uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
