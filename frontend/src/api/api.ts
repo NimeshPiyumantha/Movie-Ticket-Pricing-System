f03e46f8516c6b9cfa83ddee0556f31dee16f877
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2024-02-08 20:29:17 +0530

    Update code

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-});
-
 // const api = axios.create({
-//   baseURL: "https://ec2-54-88-109-152.compute-1.amazonaws.com/api/v1",
+//   baseURL: "http://localhost:8000/api/v1",
 // });
+
+const api = axios.create({
+  baseURL: "https://ec2-54-88-109-152.compute-1.amazonaws.com/api/v1",
+});
 
 export { api };
