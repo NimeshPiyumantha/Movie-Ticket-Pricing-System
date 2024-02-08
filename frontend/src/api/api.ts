921467264a94f215678b52d873cbd80d551ae10f
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2024-02-08 11:17:57 +0530

    Update api.ts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:8000/api/v1",
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://44.206.228.28:8000/api/v1",
 });
 
 export { api };
